abbbfd1ffe5a0621fb8c15c400a681b5cc865920
Denis Kim <denis.kim666@gmail.com>
2022-06-09 00:32:46 +0600

    add second task

--- Program.cs
+++ Program.cs
@@ -23,3 +23,25 @@
 //     }
 //         else
 //         Console.WriteLine("Числа равны");
+
+
+//============================================================================================
+        
+// SECOND TASK "Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел."
+
+
+Console.Write("Введите первое число: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+int max = num_1;
+
+if (max < num_2)
+    max = num_2;
+if (max < num_3)
+    max = num_3;
+
+Console.Write("Большее число: ");
+Console.Write(max);
